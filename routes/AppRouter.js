e23fd361b73a65dc8d2003b0207667fca497054a
Stephan D Ramalho <sramalho@fordham.edu>
2022-11-21 11:21:26 -0500

    added app router and middleware

---
+++ routes/AppRouter.js
@@ -0,0 +1,12 @@
+const Router = require('express').Router()
+const StudentRouter = require('./StudentRouter')
+const CourseRouter = require('./CourseRouter')
+const GradeRouter = require('./GradeRouter')
+const AuthRouter = require('./AuthRouter')
+
+// Router.use('/students', StudentRouter)
+Router.use('/course', CourseRouter)
+// Router.use('/grades', GradeRouter)
+Router.use('/auth', AuthRouter)
+
+module.exports = Router
