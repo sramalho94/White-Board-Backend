b1536f14d23c8bcf8f01912feb85d387fe2dc37c
Sarah Yeager <yeager.sarah.k@gmail.com>
2022-11-21 10:31:34 -0500

    course controller created

---
+++ controllers/CourseController.js
@@ -0,0 +1,35 @@
+const { Course } = require('../models')
+const middleware = require('../middleware')
+
+const GetCourse = async (req, res) => {
+  try {
+    const course = await Course.findByPk(req.params.course_id)
+    res.send(course)
+  } catch (error) {
+    throw error
+  }
+}
+
+const GetAllCourses = async (req, res) => {
+  try {
+    const courses = await Course.findAll()
+    res.send(courses)
+  } catch (error) {
+    throw error
+  }
+}
+
+const CreateCourse = async (req, res) => {
+  try {
+    const newCourse = await Course.create(req.body)
+    res.send(newCourse)
+  } catch (error) {
+    throw error
+  }
+}
+
+module.exports = {
+  GetCourse,
+  GetAllCourses,
+  CreateCourse
+}
