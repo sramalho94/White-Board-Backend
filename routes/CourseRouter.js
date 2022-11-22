a2b0a4056da4bb9721cf97d8bc578f06e1ab89c0
Sarah Yeager <yeager.sarah.k@gmail.com>
2022-11-22 09:22:37 -0500

    getallcourseswithstudents route

--- routes/CourseRouter.js
+++ routes/CourseRouter.js
@@ -3,6 +3,8 @@ const controller = require('../controllers/CourseController')
 
 router.get('/courses', controller.GetAllCourses)
 
+router.get('/courses/students', controller.GetAllCoursesWithStudents)
+
 router.get('/:course_id', controller.GetCourse)
 
 router.post('/create', controller.CreateCourse)
