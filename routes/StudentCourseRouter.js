72046ce92c19dbf196ee9f5187d753838a28e113
Stephan D Ramalho <sramalho@fordham.edu>
2022-11-21 20:37:18 -0500

    added new controller and route to studentcourses

--- routes/StudentCourseRouter.js
+++ routes/StudentCourseRouter.js
@@ -3,6 +3,8 @@ const controller = require('../controllers/StudentCourseController')
 
 router.get('/allstudentcourses', controller.GetAllStudentCourses)
 
+router.get('/student_course/:id', controller.GetStudentCoursesByCourse)
+
 router.get('/:student_course', controller.GetStudentCourse)
 
 module.exports = router
